57b8b602986a2e2decc7863bd3575f838f0e04d9
Jay Guns <jaygnz27@gmail.com>
2026-03-30 01:23:27 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import SPICalculator from './pages/SPICalculator';
+import Home from './pages/Home';
+import CodeCopy from './pages/CodeCopy';
 import TheWatchers from './pages/TheWatchers';
 import AthaVid from './pages/AthaVid';
-import Home from './pages/Home';
 import TaycanFinder from './pages/TaycanFinder';
-import CodeCopy from './pages/CodeCopy';
+import SPICalculator from './pages/SPICalculator';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
-        <Route path="/SPICalculator" element={<SPICalculator />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/CodeCopy" element={<CodeCopy />} />
         <Route path="/TheWatchers" element={<TheWatchers />} />
         <Route path="/AthaVid" element={<AthaVid />} />
-        <Route path="/Home" element={<Home />} />
         <Route path="/TaycanFinder" element={<TaycanFinder />} />
-        <Route path="/CodeCopy" element={<CodeCopy />} />
+        <Route path="/SPICalculator" element={<SPICalculator />} />
       </Routes>
     </BrowserRouter>
   )
