dcc3787312f0058420fb926b609a11af4a8d0cae
Jay Guns <jaygnz27@gmail.com>
2026-03-30 17:38:03 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import TaycanFinder from './pages/TaycanFinder';
-import TheWatchers from './pages/TheWatchers';
 import Home from './pages/Home';
 import AthaVid from './pages/AthaVid';
-import CodeCopy from './pages/CodeCopy';
-import Install from './pages/Install';
-import SPICalculator from './pages/SPICalculator';
 import Index from './pages/Index';
+import Install from './pages/Install';
+import CodeCopy from './pages/CodeCopy';
+import TaycanFinder from './pages/TaycanFinder';
+import SPICalculator from './pages/SPICalculator';
+import TheWatchers from './pages/TheWatchers';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
-        <Route path="/TaycanFinder" element={<TaycanFinder />} />
-        <Route path="/TheWatchers" element={<TheWatchers />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/AthaVid" element={<AthaVid />} />
-        <Route path="/CodeCopy" element={<CodeCopy />} />
-        <Route path="/Install" element={<Install />} />
-        <Route path="/SPICalculator" element={<SPICalculator />} />
         <Route path="/Index" element={<Index />} />
+        <Route path="/Install" element={<Install />} />
+        <Route path="/CodeCopy" element={<CodeCopy />} />
+        <Route path="/TaycanFinder" element={<TaycanFinder />} />
+        <Route path="/SPICalculator" element={<SPICalculator />} />
+        <Route path="/TheWatchers" element={<TheWatchers />} />
       </Routes>
     </BrowserRouter>
   )
