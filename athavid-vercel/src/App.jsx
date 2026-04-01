e64555432e5576ebd4844b56fedadc00855a44ef
Jay Guns <jaygnz27@gmail.com>
2026-04-01 03:10:03 +0000

    Auto-commit agent changes

--- athavid-vercel/src/App.jsx
+++ athavid-vercel/src/App.jsx
@@ -1139,7 +1139,7 @@ function VideoCard({ video, currentUser, onCommentOpen, onLike, onView, onNeedAu
               <path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"/><polyline points="16 6 12 2 8 6"/><line x1="12" y1="2" x2="12" y2="15"/>
             </svg>
           </div>
-          <div style={{ color:"#fff", fontSize:12, fontWeight:600 }}>Share</div>
+          <div style={{ color:"#fff", fontSize:12, fontWeight:600 }}>{formatCount(video.shares_count||0)}</div>
         </button>
 
 
