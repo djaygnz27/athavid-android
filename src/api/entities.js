be94f18984f4b657bd32c2bcefc332acae5a8a98
Jay Guns <jaygnz27@gmail.com>
2026-04-05 18:18:57 +0000

    Update app pages

--- src/api/entities.js
+++ src/api/entities.js
@@ -9,6 +9,13 @@ export const AthaVidComment = base44.entities.AthaVidComment;
 export const BetaTester = base44.entities.BetaTester;
 export const BugReport = base44.entities.BugReport;
 export const PasswordReset = base44.entities.PasswordReset;
+export const SachiVideo = base44.entities.SachiVideo;
+export const SachiComment = base44.entities.SachiComment;
+export const Follow = base44.entities.Follow;
+export const SachiPodcast = base44.entities.SachiPodcast;
+export const SachiPodcastEpisode = base44.entities.SachiPodcastEpisode;
+export const UserInterest = base44.entities.UserInterest;
+export const SachiAnalytics = base44.entities.SachiAnalytics;
 
 // auth sdk:
 export const User = base44.auth;
