eee06adb0aec76d4a6b84c6dbb0259364c1ee1e7
Jay Guns <jaygnz27@gmail.com>
2026-04-24 02:38:16 +0000

    Update app pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,32 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Install from './pages/Install';
+import JMUXDashboard from './pages/JMUXDashboard';
+import SPICalculator from './pages/SPICalculator';
+import TaycanFinder from './pages/TaycanFinder';
+import TheWatchers from './pages/TheWatchers';
 import Dashboard from './pages/Dashboard';
 import Home from './pages/Home';
 import Sachi from './pages/Sachi';
 import AthaVid from './pages/AthaVid';
 import Index from './pages/Index';
 import CodeCopy from './pages/CodeCopy';
-import JMUXDashboard from './pages/JMUXDashboard';
-import Install from './pages/Install';
-import TheWatchers from './pages/TheWatchers';
-import TaycanFinder from './pages/TaycanFinder';
-import SPICalculator from './pages/SPICalculator';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/Home" replace />} />
+        <Route path="/Install" element={<Install />} />
+        <Route path="/JMUXDashboard" element={<JMUXDashboard />} />
+        <Route path="/SPICalculator" element={<SPICalculator />} />
+        <Route path="/TaycanFinder" element={<TaycanFinder />} />
+        <Route path="/TheWatchers" element={<TheWatchers />} />
         <Route path="/Dashboard" element={<Dashboard />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Sachi" element={<Sachi />} />
         <Route path="/AthaVid" element={<AthaVid />} />
         <Route path="/Index" element={<Index />} />
         <Route path="/CodeCopy" element={<CodeCopy />} />
-        <Route path="/JMUXDashboard" element={<JMUXDashboard />} />
-        <Route path="/Install" element={<Install />} />
-        <Route path="/TheWatchers" element={<TheWatchers />} />
-        <Route path="/TaycanFinder" element={<TaycanFinder />} />
-        <Route path="/SPICalculator" element={<SPICalculator />} />
       </Routes>
     </BrowserRouter>
   )
